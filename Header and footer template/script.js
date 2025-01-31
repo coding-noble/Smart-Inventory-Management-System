625cd5de976b156ddfa4e77391dfc632d24afab2
TheTubbyRaider <grsraider66@gmail.com>
2025-01-30 19:00:22 -0700

    Header and footer templates

---
+++ Header and footer template/script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const sideMenu = document.getElementById("sideMenu");
+    sideMenu.classList.toggle("open");
+  }
+  
+  function toggleSearch() {
+    const searchIcon = document.querySelector(".search-icon");
+    const searchBox = document.getElementById("searchBox");
+  
+    if (searchBox.classList.contains("show-input")) {
+      searchBox.classList.remove("show-input");
+      searchIcon.style.transform = "rotate(0deg)";
+    } else {
+      searchBox.classList.add("show-input");
+      searchIcon.style.transform = "rotate(90deg)";
+    }
+  }
